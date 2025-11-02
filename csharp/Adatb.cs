e5e8f3fb73d3776baf37d0fdb50a8f3432c8002c
Benjamin Stonawski <bendzsike6@gmail.com>
2025-11-02 19:05:30 +0100

    Add files via upload

---
+++ csharp/Adatb.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HovaTovabbCsharp
+{
+    public class Felhasznalo
+    {
+        public string Felhasznalonev { get; set; }
+        public string Jelszo { get; set; }
+        public string TeljesNev { get; set; }
+        public string Email { get; set; }
+        public Kedvezmeny Kedvezmeny { get; set; }
+
+        public override string ToString() => $"{TeljesNev} ({Felhasznalonev})";
+    }
+
+    public class Kedvezmeny
+    {
+        public int Id { get; set; }
+        public string Nev { get; set; }
+        public int Szazalek { get; set; }
+    }
+
+    public class Jarmu
+    {
+        public int Id { get; set; }
+        public string Tipus { get; set; }
+    }
+
+    public class Jarat
+    {
+        public int Id { get; set; }
+        public string IndAllomas { get; set; }
+        public string ErkAllomas { get; set; }
+        public DateTime IndIdo { get; set; }
+        public DateTime ErkIdo { get; set; }
+        public decimal Jegyar { get; set; }
+        public Jarmu Jarmu { get; set; }
+    }
+    public class TervJarat
+    {
+        public int Sorrend { get; set; }
+        public Jarat Jarat { get; set; }
+    }
+
+    public class Terv
+    {
+        public int Id { get; set; }
+        public Felhasznalo Felhasznalo { get; set; }
+        public List<TervJarat> Jaratok { get; set; } = new List<TervJarat>();
+    }
+
+    public class Admin
+    {
+        public string Felhasznalonev { get; set; }
+        public string Jelszo { get; set; }
+    }
+}
---
+++ csharp/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HovaTovabbCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var diak = new Kedvezmeny { Id = 1, Nev = "Diák", Szazalek = 50 };
+
+            var benjike = new Felhasznalo
+            {
+                Felhasznalonev = "benjike",
+                Jelszo = "tesztelek",
+                TeljesNev = "Stonawski Benjamin",
+                Email = "bendzsike6@gmail.com",
+                Kedvezmeny = diak
+            };
+
+            var busz = new Jarmu { Id = 1, Tipus = "Busz" };
+            var vonat = new Jarmu { Id = 2, Tipus = "Vonat" };
+
+            var j1 = new Jarat
+            {
+                Id = 1,
+                IndAllomas = "Mátészalka",
+                ErkAllomas = "Nyíregyháza",
+                IndIdo = new DateTime(2025, 11, 5, 7, 30, 0),
+                ErkIdo = new DateTime(2025, 11, 5, 9, 0, 0),
+                Jegyar = 560,
+                Jarmu = busz
+            };
+
+            var j2 = new Jarat
+            {
+                Id = 2,
+                IndAllomas = "Nyíregyháza",
+                ErkAllomas = "Budapest-Nyugati",
+                IndIdo = new DateTime(2025, 11, 5, 9, 45, 0),
+                ErkIdo = new DateTime(2025, 11, 5, 13, 0, 0),
+                Jegyar = 2500,
+                Jarmu = vonat
+            };
+
+            var terv1 = new Terv
+            {
+                Id = 1,
+                Felhasznalo = benjike,
+                Jaratok = new List<TervJarat>
+                {
+                    new TervJarat { Sorrend = 1, Jarat = j1 },
+                    new TervJarat { Sorrend = 2, Jarat = j2 }
+                }
+            };
+
+            var j3 = new Jarat
+            {
+                Id = 3,
+                IndAllomas = "Mátészalka",
+                ErkAllomas = "Fehérgyarmat",
+                IndIdo = new DateTime(2025, 11, 10, 8, 0, 0),
+                ErkIdo = new DateTime(2025, 11, 10, 8, 45, 0),
+                Jegyar = 420,
+                Jarmu = busz
+            };
+
+            var j4 = new Jarat
+            {
+                Id = 4,
+                IndAllomas = "Fehérgyarmat",
+                ErkAllomas = "Csenger",
+                IndIdo = new DateTime(2025, 11, 10, 9, 0, 0),
+                ErkIdo = new DateTime(2025, 11, 10, 9, 35, 0),
+                Jegyar = 560,
+                Jarmu = busz
+            };
+
+            var terv2 = new Terv
+            {
+                Id = 2,
+                Felhasznalo = benjike,
+                Jaratok = new List<TervJarat>
+                {
+                    new TervJarat { Sorrend = 1, Jarat = j3 },
+                    new TervJarat { Sorrend = 2, Jarat = j4 }
+                }
+            };
+
+            var felhasznalok = new List<Felhasznalo> { benjike };
+            var tervek = new List<Terv> { terv1, terv2 };
+
+            Console.WriteLine("--- Bejelentkezés ---");
+            Console.Write("Felhasználónév: ");
+            string fn = Console.ReadLine();
+            Console.Write("Jelszó: ");
+            string pw = Console.ReadLine();
+
+            var bejelentkezett = felhasznalok.FirstOrDefault(f => f.Felhasznalonev == fn && f.Jelszo == pw);
+
+            if (bejelentkezett == null)
+            {
+                Console.WriteLine("\nHibás felhasználónév vagy jelszó!");
+                return;
+            }
+
+            Console.WriteLine($"\nÜdvözöllek, {bejelentkezett.TeljesNev}!");
+            Console.WriteLine($"Email: {bejelentkezett.Email}");
+            Console.WriteLine($"Kedvezmény: {bejelentkezett.Kedvezmeny.Nev} ({bejelentkezett.Kedvezmeny.Szazalek}%)\n");
+
+            var sajatTervek = tervek.Where(t => t.Felhasznalo == bejelentkezett).ToList();
+
+            if (!sajatTervek.Any())
+            {
+                Console.WriteLine("Nincs elérhető terved!");
+                return;
+            }
+
+            foreach (var terv in sajatTervek)
+            {
+                Console.WriteLine($"--- Terv #{terv.Id} ---");
+                foreach (var tj in terv.Jaratok.OrderBy(x => x.Sorrend))
+                {
+                    var j = tj.Jarat;
+                    Console.WriteLine($"{tj.Sorrend}. {j.IndAllomas} → {j.ErkAllomas}");
+                    Console.WriteLine($"   Indulás: {j.IndIdo:yyyy.MM.dd. HH:mm}, Érkezés: {j.ErkIdo:HH:mm}");
+                    Console.WriteLine($"   Jármű: {j.Jarmu.Tipus}, Jegyár: {j.Jegyar} Ft\n");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
